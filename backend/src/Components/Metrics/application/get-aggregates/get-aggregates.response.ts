cc60cbe3b3895f21c04c5eef57d74d067044f57c
joav <jim.acero.valcarcel@gmail.com>
2025-06-15 06:05:04 -0500

    BE: GetAggregatesResponse implementation

--- backend/src/Components/Metrics/application/get-aggregates/get-aggregates.response.ts
+++ backend/src/Components/Metrics/application/get-aggregates/get-aggregates.response.ts
@@ -1,5 +1,10 @@
 import { Response } from '@Components/Shared/domain/response';
+import { Aggregate } from '@Components/Metrics/domain/aggregate';
 
 export class GetAggregatesResponse implements Response {
-
+  readonly aggregates: AggregateResponse[];
+  constructor(aggregates: Aggregate[]) {
+    this.aggregates = aggregates.map(a => a.toPrimitives());
+  }
 }
+
