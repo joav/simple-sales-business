46c4441fec8bc3be690ba67d0c5d2ec7f24758f2
joav <jim.acero.valcarcel@gmail.com>
2025-06-15 06:08:51 -0500

    BE: GetAggregatesQuery

---
+++ backend/src/Components/Metrics/application/get-aggregates/get-aggregates.query.ts
@@ -0,0 +1,4 @@
+import { Query } from '@Components/Shared/domain/query';
+import { GetAggregatesResponse } from './get-aggregates.response';
+
+export class GetAggregatesQuery implements Query<GetAggregatesResponse> {}
