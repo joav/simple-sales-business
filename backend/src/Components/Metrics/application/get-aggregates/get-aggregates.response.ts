aae56ca88428b068a85538dfe39f2bb4a2f31d82
joav <jim.acero.valcarcel@gmail.com>
2025-06-15 06:50:03 -0500

    BE: Refactor GetAggregatesResponse

--- backend/src/Components/Metrics/application/get-aggregates/get-aggregates.response.ts
+++ backend/src/Components/Metrics/application/get-aggregates/get-aggregates.response.ts
@@ -1,6 +1,12 @@
 import { Response } from '@Components/Shared/domain/response';
 import { Aggregate } from '@Components/Metrics/domain/aggregate';
 
+type AggregateResponse = {
+  aggregateId: string;
+  aggregateFn: string;
+  category: string;
+};
+
 export class GetAggregatesResponse implements Response {
   readonly aggregates: AggregateResponse[];
   constructor(aggregates: Aggregate[]) {
