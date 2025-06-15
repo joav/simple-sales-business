cdb9014546909aa2cf119bc23273c71dbb4940db
joav <jim.acero.valcarcel@gmail.com>
2025-06-15 06:40:09 -0500

    BE: Refactor GetAggregatesQueryHandler

--- backend/src/Components/Metrics/application/get-aggregates/get-aggregates.query-handler.ts
+++ backend/src/Components/Metrics/application/get-aggregates/get-aggregates.query-handler.ts
@@ -3,7 +3,7 @@ import { AggregatesGetter } from './aggregates-getter';
 import { GetAggregatesResponse } from './get-aggregates.response';
 import { GetAggregatesQuery } from './get-aggregates.query';
 
-export class GetAggregatesQueryHandler implements QueryHandler<GetAggregatesQuery<GetAggregatesResponse>> {
+export class GetAggregatesQueryHandler implements QueryHandler<GetAggregatesQuery, GetAggregatesResponse> {
   constructor(private getter: AggregatesGetter) {}
   async handle(_query: GetAggregatesQuery) {
     const aggregates = await this.getter.run();
