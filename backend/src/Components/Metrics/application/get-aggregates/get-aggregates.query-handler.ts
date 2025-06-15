92f385e00cb4d5aff0f145f6922271ad755c871b
joav <jim.acero.valcarcel@gmail.com>
2025-06-15 06:28:44 -0500

    BE: GetAggregatesQueryHandler

---
+++ backend/src/Components/Metrics/application/get-aggregates/get-aggregates.query-handler.ts
@@ -0,0 +1,12 @@
+import { QueyHandler } '@Components/Shared/domain/query-handler';
+import { AggregatesGetter } from './aggregates-getter';
+import { GetAggregatesResponse } from './get-aggregates.response';
+import { GetAggregatesQuery } from './get-aggregates.query';
+
+export class GetAggregatesQueryHandler implements QueryHandler<GetAggregatesQuery<GetAggregatesResponse>> {
+  constructor(private getter: AggregatesGetter) {}
+  async handle(_query: GetAggregatesQuery<GetAggregatesResponse>) {
+    const aggregates = await this.getter.run();
+    return new GetAggregatesResponse(aggregates);
+}
+}
