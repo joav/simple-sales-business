020db157c0858831f629412c9ae71e7eafd30a61
joav <jim.acero.valcarcel@gmail.com>
2025-06-14 22:05:01 -0500

    BE: Refactor Get Aggregates test

--- backend/test/Components/Metrics/application/get-aggregates.test.ts
+++ backend/test/Components/Metrics/application/get-aggregates.test.ts
@@ -1,3 +1,20 @@
+import { GetAggregatesQuery } from '@Components/Metrics/application/get-aggregates/get-aggregates.query.ts';
+import { GetAggregatesQueryHandler } from '@Components/Metrics/application/get-aggregates/get-aggregates.query-handler.ts';
+import { AggregatesGetter } from '@Components/Metrics/application/get-aggregates/aggregates-getter.ts';
+import { AggregatesRepository } from '@Components/Metrics/domain/aggregates.repository';
+import { Aggregate } from '@Components/Metrics/domain/aggregate';
+
 describe('Get Aggregates', () => {
-  it('should handle query and get response');
+  it('should handle query and get response', async () => {
+    const repo: AggregatesRepository = {
+      listAggregates: () => Promise.resolve([Aggregate.fromPrimitives({aggregateId: 'some', aggregateFn: 'RECOUNT', category: 'products')])
+    };
+    const getter = new AggregatesGetter(repo);
+    const handler = new GetAggregatesQueryHandler(getter);
+    const query = new GetAggregatesQuery();
+
+    const result = await handler.handle(query);
+    expect(result.aggregates).toBeTruthy();
+    expect(result.aggregates.length).toEqual(1);
+  });
 });
