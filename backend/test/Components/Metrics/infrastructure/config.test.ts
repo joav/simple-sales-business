ae58e93f2238027cb0245c4a8d9719c5f34f379e
joav <jim.acero.valcarcel@gmail.com>
2025-06-15 22:19:31 -0500

    BE: Config tests

---
+++ backend/test/Components/Metrics/infrastructure/config.test.ts
@@ -0,0 +1,21 @@
+import { Container } from 'inversify';
+import MetrictsConfigExecutor from '@Components/Metrics/infrastructure/config/metrics.config-executor';
+import { GetAggregatesQueryHandler } from '@Components/Metrics/application/get-aggregates/get-aggregates.query-handler';
+
+
+describe('Metrics Config', () => {
+  let container: Container;
+  beforeEach(() => {
+    container = new Container();
+    MetrictsConfigExecutor.config(container);
+  });
+  it('should QUERY_HANDLER resolved', () => {
+    const handlers = container.getAll(sharedIdentifiers.QUERY_HANDLER);
+    expect(handlers).toBeTruthy();
+    expect(handlers.length).toBeTruthy();
+  });
+  it('should GetAggregatesQueryHandler resolved', () => {
+    const handler = container.get(GetAggregatesQueryHandler);
+    expect(handler).toBeTruthy();
+  });
+});
