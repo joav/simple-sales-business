f464ef568e0874867c8e22f1ba6123480e13aaa6
joav <jim.acero.valcarcel@gmail.com>
2025-06-17 06:38:36 -0500

    BE: Types

--- backend/src/Components/Metrics/infrastructure/config/aggregates.container-module-creator.ts
+++ backend/src/Components/Metrics/infrastructure/config/aggregates.container-module-creator.ts
@@ -3,6 +3,7 @@ import { ContainerModuleCreator} from '@Components/Shared/infrastructure/contain
 import { AggregatesGetter } from '@Components/Metrics/application/get-aggregates/aggregates-getter';
 import { GetAggregatesQueryHandler } from '@Components/Metrics/application/get-aggregates/get-aggregates.query-handler';
 import sharedIdentifiers from '@Components/Shared/infrastructure/di-identifiers';
+import { AggregatesRepository } from '@Components/Metrics/domain/aggregates.repository';
 
 export default {
   create() {
@@ -11,9 +12,9 @@ export default {
       options.bind(repoSymbol).toConstantValue({
       listAggregates: () => Promise.resolve([])
 });
-      options.bind(AggregatesGetter).toResolvedValue((repo) => new AggregatesGetter(repo), [repoSymbol]);
-      options.bind(GetAggregatesQueryHandler).toResolvedValue((getter) => new GetAggregatesQueryHandler(getter), [AggregatesGetter]);
-      options.bind(sharedIdentifiers.QUERY_HANDLER).toResolvedValue(handler => handler, [GetAggregatesQueryHandler]);
+      options.bind(AggregatesGetter).toResolvedValue((repo: AggregatesRepository) => new AggregatesGetter(repo), [repoSymbol]);
+      options.bind(GetAggregatesQueryHandler).toResolvedValue((getter: AggregatesGetter) => new GetAggregatesQueryHandler(getter), [AggregatesGetter]);
+      options.bind(sharedIdentifiers.QUERY_HANDLER).toResolvedValue((handler: GetAggregatesQueryHandler) => handler, [GetAggregatesQueryHandler]);
     });
   }
 } satisfies ContainerModuleCreator;
