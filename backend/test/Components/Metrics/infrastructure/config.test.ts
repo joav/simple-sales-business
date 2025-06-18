9241fffef1e41354266ce2e74f7a11cdf7f48a01
joav <jim.acero.valcarcel@gmail.com>
2025-06-17 22:06:58 -0500

    BE: Update Metrics tests

--- backend/test/Components/Metrics/infrastructure/config.test.ts
+++ backend/test/Components/Metrics/infrastructure/config.test.ts
@@ -4,12 +4,21 @@ import { GetAggregatesQueryHandler } from '@Components/Metrics/application/get-a
 import sharedIdentifiers from '@Components/Shared/infrastructure/di-identifiers';
 import metricsDiIdentifiers from '@Components/Metrics/infrastructure/config/di-identifiers';
 import { AggregatesInMemoryRepository } from '@Components/Metrics/infrastructure/data/in-memory/aggregates.in-memory.repository';
+import { QueryBus } from '@Components/Shared/domain/query-bus';
+import { AggregatesRoutes } from '@Components/Metrics/infrastructure/web/Routes/aggregates.routes';
 
+class MyQueryBus implements QueryBus {
+  ask() {
+    return Promise.resolve();
+  }
+}
 
 describe('Metrics Config', () => {
   let container: Container;
   beforeEach(() => {
     container = new Container();
+
+    container.bind(sharedIdentifiers.QUERY_BUS).to(MyQueryBus);
     MetrictsConfigExecutor.config(container);
     container.unbind(metricsDiIdentifiers.AGGREGATES_REPOSITORY);
     container.bind(metricsDiIdentifiers.AGGREGATES_REPOSITORY).to(AggregatesInMemoryRepository);
@@ -22,5 +31,9 @@ describe('Metrics Config', () => {
   it('should GetAggregatesQueryHandler resolved', () => {
     const handler = container.get(GetAggregatesQueryHandler);
     expect(handler).toBeTruthy();
+  });
+  it('should AggregatesRoutes resolved', () => {
+    const routes = container.get(AggregatesRoutes);
+    expect(routes).toBeTruthy();
   });
 });
