835b9862017f821ec98124581eaa099d73277f70
joav <jim.acero.valcarcel@gmail.com>
2025-06-21 21:06:58 -0500

    BE: AggregateValue

---
+++ backend/src/Components/Metrics/domain/aggregate-value.ts
@@ -0,0 +1,45 @@
+import { Aggregate } from './aggregate';
+import { strToDateOrError } from './str-to-date-or-error';
+
+export class AggregateValue extends Aggregate {
+  private constructor(
+    public readonly aggregateId: string,
+    public readonly category: Category,
+    public readonly aggregateFn: AggregateFn,
+    public readonly aggregateValue: number;
+    public readonly lastUpdate: Date
+  ) {
+    super(
+      aggregateId,
+      category,
+      aggregateFn
+    );
+  }
+  toPrimitives() {
+    return {
+      aggregateId: this.aggregateId,
+      aggregateFn: this.aggregateFn as string,
+      category: this.category as string,
+      aggregateValue: this.aggregateValue,
+      lastUpdate: this.lastUpdate.toISOString()
+    };
+  }
+  static fromPrimitives(values: { aggregateId: string; category: string; aggregateFn: string, aggregateValue: number, lastUpdate: string }) {
+    const aggregateId = Aggregate.verifyAggregateId(values.aggregateId);
+    const category = categoryFromPrimitive(values.category);
+    const aggregateFn = Aggregate.verifyAggregateFn(values.aggregateFn);
+    const aggregateValue = AggregateValue.verifyAggregateValue(values.aggregateValue);
+    const lastUpdate = strToDateOrError(valides.lastUpdate, AggregateValue.lastUpdateErrorFactory);
+    return new AggregateValue(aggregateId, category, aggregateFn, aggregateValue, lastUpdate);
+  }
+
+  private static verifyAggregateValue(aggregateValue: number) {
+    if (isNaN(aggregateValue)) throw new Error();
+    return +aggregateValue;
+  }
+
+  private static lastUpdateErrorFactory() {
+    return new Error
+  }
+}
+
