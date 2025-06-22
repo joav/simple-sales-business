6c0bd8be07743c151faac6b08d60b47a54e75b9d
joav <jim.acero.valcarcel@gmail.com>
2025-06-22 07:25:12 -0500

    BE: GetAggregateValueController

---
+++ backend/src/Components/Metrics/infrastructure/web/Controllers/get-aggregate-value.controller.ts
@@ -0,0 +1,21 @@
+import { Controller } from '@Components/Shared/infrastructure/web/Controller';
+import { successResponse } from '@Components/Shared/infrastructure/web/responses/success-response';
+import { Request, Response } from 'express';
+import { inject, injectable } from 'inversify';
+import { StatusCodes } from 'http-status-codes';
+import sharedDiIdentifiers from '@Components/Shared/infrastructure/di-identifiers';
+import { QueryBus } from '@Components/Shared/domain/query-bus';
+import { GetAggregateValueQuery } from '@Components/Metrics/application/get-aggregate-value/get-aggregate-value.query';
+import { GetAggregateValueResponse } from '@Components/Metrics/application/get-aggregates/get-aggregate-value.response';
+
+@injectable('Singleton')
+export class GetAggregateValueController implements Controller {
+  constructor(@inject(sharedDiIdentifiers.QUERY_BUS) private queryBus: QueryBus) {}
+
+  handleRequest = async (req: Request, res: Response) => {
+    const query = new GetAggregateValueQuery(req.params.category, req.params.aggregateId);
+    const result = await this.queryBus.ask<GetAggregateValueResponse>(query);
+    const response = successResponse(result.aggregateValue, StatusCodes.OK);
+    res.status(response.status.httpStatusCode).json(response);
+  };
+}
