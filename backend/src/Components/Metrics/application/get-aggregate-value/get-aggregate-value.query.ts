4290e82f9b31974a028aab5fea5760c42611fe5d
Jimmy Acero <jim.acero.valcarcel@gmail.com>
2025-06-22 06:40:26 -0500

    BE: Prettier

--- backend/src/Components/Metrics/application/get-aggregate-value/get-aggregate-value.query.ts
+++ backend/src/Components/Metrics/application/get-aggregate-value/get-aggregate-value.query.ts
@@ -2,5 +2,8 @@ import { Query } from '@Components/Shared/domain/query';
 import { GetAggregateValueResponse } from './get-aggregate-value.response';
 
 export class GetAggregateValueQuery implements Query<GetAggregateValueResponse> {
-  constructor(public readonly category: string, public readonly aggregateId: string) {}
+  constructor(
+    public readonly category: string,
+    public readonly aggregateId: string
+  ) {}
 }
