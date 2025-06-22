7ce9a130c4637824f0d93402e4fe056b8d90d292
joav <jim.acero.valcarcel@gmail.com>
2025-06-22 07:15:49 -0500

    BE: AggregateValuesRepository un memory implemetation

--- backend/src/Components/Metrics/infrastructure/data/in-memory/aggregates.in-memory.repository.ts
+++ backend/src/Components/Metrics/infrastructure/data/in-memory/aggregates.in-memory.repository.ts
@@ -1,7 +1,9 @@
 import { Aggregate } from '@Components/Metrics/domain/aggregate';
 import { AggregatesRepository } from '@Components/Metrics/domain/aggregates.repository';
+import { AggregateValue } from '@Components/Metrics/domain/aggregate-value';
+import { AggregateValuesRepository } from '@Components/Metrics/domain/aggregate-values.repository';
 
-export class AggregatesInMemoryRepository implements AggregatesRepository {
+export class AggregatesInMemoryRepository implements AggregatesRepository, AggregateValuesRepository {
   private aggregates: Aggregate[] = [
     Aggregate.fromPrimitives({
       aggregateId: 'some-count',
@@ -9,8 +11,19 @@ export class AggregatesInMemoryRepository implements AggregatesRepository {
       category: 'products'
     })
   ];
+  private aggregateValue = AggregateValue.fromPrimitives({
+    aggregateId: 'some-count',
+    aggregateFn: 'RECOUNT',
+    category: 'products',
+    aggregateValue: 5,
+    lastUpdate: (new Date).toISOString()
+});
 
   listAggregates(): Promise<Aggregate[]> {
     return Promise.resolve(this.aggregates);
+  }
+
+  get(): Promise<AggregateValue> {
+    return Promise.resolve(this.aggregateValue);
   }
 }
