61f626b0c19c3465f494fbdd388f3e1c7a032650
joav <jim.acero.valcarcel@gmail.com>
2025-06-21 21:22:27 -0500

    BE: str-to-date-or-error

---
+++ backend/src/Components/Metrics/domain/str-to-date-or-error.ts
@@ -0,0 +1,11 @@
+import { DomainException } from '@Components/Shared/domain/exceptions/domain-exception';
+
+export const strToDateOrError = (dateStr: string, errorFactory: () => DomainException) => {
+  const dateObject = new Date(dateStr);
+
+  if (isNaN(dateObject.getTime()) || dateObject.toISOString() === dateStr) {
+    throw errorFactory();
+  }
+
+  return dateObject;
+};
