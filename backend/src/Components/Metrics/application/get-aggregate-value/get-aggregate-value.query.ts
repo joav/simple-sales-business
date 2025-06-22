fda5e0ce403647aba27f350fcaec04a6734e63a1
joav <jim.acero.valcarcel@gmail.com>
2025-06-22 06:29:24 -0500

    BE: GetAggregateValueQuery

---
+++ backend/src/Components/Metrics/application/get-aggregate-value/get-aggregate-value.query.ts
@@ -0,0 +1,6 @@
+import { Query } from '@Components/Shared/domain/query';
+import { GetAggregateValueResponse } from './get-aggregate-value.response';
+
+export class GetAggregateValueQuery implements Query<GetAggregateValueResponse> {
+  constructor(public readonly category: string, public readonly aggregateId: string) {}
+}
