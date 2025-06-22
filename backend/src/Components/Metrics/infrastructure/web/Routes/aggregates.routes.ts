5ed403da2a7c773115ed5aa2ee32ff2e208d5f93
joav <jim.acero.valcarcel@gmail.com>
2025-06-22 07:30:44 -0500

    BE: Get aggregate value route

--- backend/src/Components/Metrics/infrastructure/web/Routes/aggregates.routes.ts
+++ backend/src/Components/Metrics/infrastructure/web/Routes/aggregates.routes.ts
@@ -1,12 +1,15 @@
 import { AbstractRoutes } from '@Components/Shared/infrastructure/web/AbstractRoutes';
 import { inject, injectable } from 'inversify';
 import { GetAggregatesController } from '../Controllers/get-aggregates.controller';
+import { GetAggregateValueController } from '../Controllers/get-aggregate-value.controller';
 
 @injectable('Singleton')
 export class AggregatesRoutes extends AbstractRoutes {
   constructor(
     @inject(GetAggregatesController)
-    private readonly getAggregatesController: GetAggregatesController
+    private readonly getAggregatesController: GetAggregatesController,
+    @inject(GetAggregateValueController)
+    private readonly getAggregateValueController: GetAggregateValueController
   ) {
     super();
     this.setRoutes();
@@ -14,5 +17,6 @@ export class AggregatesRoutes extends AbstractRoutes {
 
   protected setRoutes() {
     this.router.get('', this.getAggregatesController.handleRequest);
+    this.router.get('/:aggregateId', this.getAggregateValueController.handleRequest);
   }
 }
