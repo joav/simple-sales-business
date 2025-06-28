f00ee728bef019767c73ab2d7c9006f6f8b497f2
Jimmy Acero <jim.acero.valcarcel@gmail.com>
2025-06-27 21:33:59 -0500

    BE: Ranking

--- backend/src/Components/Metrics/Rankings/domain/ranking.ts
+++ backend/src/Components/Metrics/Rankings/domain/ranking.ts
@@ -1,1 +1,52 @@
-export class Ranking {}
+import { Category, categoryFromPrimitive } from '@Metrics/Shared/domain';
+import { InvalidInputException } from '@Shared/domain';
+
+export const RANKING_EXCEPTIONS = {
+  InvalidRankingSlug: {
+    statusCode: 4000,
+    statusMessage: 'Metrics:Domain:Ranking:InvalidRankingSlug'
+  },
+  InvalidRankingValueTitle: {
+    statusCode: 4001,
+    statusMessage: 'Metrics:Domain:Ranking:InvalidRankingValueTitle'
+  }
+};
+
+export class Ranking {
+  private constructor(
+    public readonly rankingSlug: string,
+    public readonly rankingValueTitle: string,
+    public readonly category: Category
+  ) {}
+
+  toPrimitives() {
+    return {
+      rankingSlug: this.rankingSlug,
+      rankingValueTitle: this.rankingValueTitle,
+      category: this.category as string
+    };
+  }
+
+  static fromPrimitives(values: {
+    rankingSlug: string;
+    rankingValueTitle: string;
+    category: string;
+  }) {
+    const rankingSlug = Ranking.verifRankingSlug(values.rankingSlug);
+    const rankingValueTitle = Ranking.verifRankingValueTitle(values.rankingValueTitle);
+    const category = categoryFromPrimitive(values.category);
+    return new Ranking(rankingSlug, rankingValueTitle, category);
+  }
+
+  protected static verifyRankingSlug(rankingSlug: string): string {
+    if (!rankingSlug)
+      throw InvalidInputException.fromStatusParams(RANKING_EXCEPTIONS.InvalidRankingSlug);
+    return rankingSlug;
+  }
+
+  protected static verifyRankingValueTitle(rankingValueTitle: string): string {
+    if (!rankingValueTitle)
+      throw InvalidInputException.fromStatusParams(RANKING_EXCEPTIONS.InvalidRankingValueTitle);
+    return rankingValueTitle;
+  }
+}
